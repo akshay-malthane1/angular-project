2f4f9465dc3f5732de6f22cc1c762f05cf218503
akshay-malthane1 <akshay.malthane07@gmail.com>
2020-10-18 06:43:08 +0100

    error handeling globally

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { from, Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { ErrorService } from './error.service';
 
 
+class Post {
+  public username: string;
+  public email: string;
+  public password: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
- 
-  constructor(private _http: HttpClient) { } 
 
-  createUser(user){
-    return this._http.post("http://localhost:3000/posts", user );
+  constructor(private _http: HttpClient, private errorservice: ErrorService
+  ) { }
+
+  createUser(user): Observable<Post[]> {
+    return this._http.post<Post[]>("http://localhost:3000/posts", user);
   };
-  getAllUser(){
+
+  getAllUser() {
     return this._http.get("http://localhost:3000/posts")
+      .pipe(
+        retry(1),
+        catchError(this.errorservice.handleError)
+      );
+
   };
-  deleteUser(user){
-    return this._http.delete("http://localhost:3000/posts/" +user.id)
+
+  deleteUser(user) {
+    return this._http.delete("http://localhost:3000/posts/" + user.id)
   }
-  updateUser(user){
-    return this._http.put("http://localhost:3000/posts/" +user.id, user)
+  updateUser(user) {
+    return this._http.put("http://localhost:3000/posts/" + user.id, user)
   }
 }
---
+++ src/app/error.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+class Post {
+  constructor(
+    public id: string,
+    public title: string,
+    public body: string
+  ) { }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+  constructor(private http: HttpClient) { }
+
+  
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+  
+}
