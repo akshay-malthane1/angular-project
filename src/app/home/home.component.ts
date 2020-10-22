f1efa2d8f9dc373c3573187bddc4d16735eec1ac
akshay-malthane1 <akshay.malthane07@gmail.com>
2020-10-23 00:21:02 +0530

    added background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   dataArray = [
-    {src: "assets/guitar2.jpg", title: 'Buy Electric Guitar', price: "$17.99"},
-    {src: "assets/guitar3.jpg", title: 'Buy Electric Guitar', price: "$17.99"},
-    {src: "assets/guitar10.jpg", title: 'Buy Electric Guitar', price: "$17.99"},
-    {src: "assets/guitar5.jpg", title: 'Buy Electric Guitar', price: "$17.99"},
-    {src: "assets/guitar6.jpg", title: 'Buy Electric Guitar', price: "$17.99"},
-    {src: "assets/guitar7.jpg", title: 'Buy Electric Guitar', price: "$17.99"}     
+    {src: "assets/guitar2.jpg", title: 'Buy Electric Guitar', price: "$19.99"},
+    {src: "assets/guitar3.jpg", title: 'Buy Electric Guitar', price: "$23.99"},
+    {src: "assets/guitar10.jpg", title: 'Buy Electric Guitar', price: "$25.99"},
+    {src: "assets/guitar5.jpg", title: 'Buy Electric Guitar', price: "$21.99"},
+    {src: "assets/guitar6.jpg", title: 'Buy Electric Guitar', price: "$28.99"},
+    {src: "assets/guitar7.jpg", title: 'Buy Electric Guitar', price: "$18.99"}     
    ];;
   constructor() { }
   ngOnInit(): void {    
